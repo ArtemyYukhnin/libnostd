110e88ba0e91f24efadfaf6033051d99f7241005
st141054 <st141054@spbu.ru>
2026-04-14 12:56:36 +0300

    feat: add core cs and math

---
+++ src/core/common.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+void credits() {
+	std::cout << "libnostd v0.0.1 - A Powerful C++ Library for "
+		     "High-Perfomance Computing\n";
+	std::cou << "Licensed by MIT License\n;"
+	std::cout << "Developed&maintained by @ArtemyYuhknin\n";
+	std::cout << '\n';
+}
