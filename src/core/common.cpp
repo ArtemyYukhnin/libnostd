a0d334d3a5e872d93abb7dea12d89b9d7470311a
st141054 <st141054@spbu.ru>
2026-05-11 17:14:06 +0300

    feat: create shared library

--- src/core/common.cpp
+++ src/core/common.cpp
@@ -13,7 +13,7 @@
 void credits() {
 	std::cout << "libnostd v0.0.1 - A Powerful C++ Library for "
 		     "High-Perfomance Computing\n";
-	std::cou << "Licensed by MIT License\n;"
+	std::cout << "Licensed by MIT License\n;";
 	std::cout << "Developed&maintained by @ArtemyYuhknin\n";
 	std::cout << '\n';
 }
